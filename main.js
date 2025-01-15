ef52fc0d2c31585b062c944eef70e28cfce43db8
ansarali11 <mdansarali4411@gmail.com>
2025-01-15 11:22:15 +0530

    added

---
+++ main.js
@@ -0,0 +1,21 @@
+$(window).on("scroll", function() {
+    var scrollTop= $(window).scrollTop();
+    if(scrollTop >= 80)
+    {
+        $('body').addClass('fixed-header');
+    }
+    else{
+        $('body').removeClass('fixed-header');
+    }
+});
+
+$(document).ready(function() {
+
+    new Typed('#type-it', {
+        strings: ['Designer', 'Developer'],
+        typeSpeed: 100,
+        loop:true
+      });
+})
+
+
